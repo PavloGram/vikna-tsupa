da7904e22e6deec517fb55007c39a7eabe55877a
PavloGram <113470072+PavloGram@users.noreply.github.com>
2024-04-19 12:54:01 +0300

    add drop list

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -32,40 +32,102 @@ function Header() {
               </a>
             </li>
             <li>
-              <a href="/service" className="nav-link">
-                ремонт та обслуговування
-              </a>
+              <div  className="nav-link  drop-list">
+                ремонт та обслуговування  <span className="treangel">⏷</span>
+                <div className="drop-down-list-wrap">
+                  <ul className="drop-down-list">
+                    <li><a href="/service" className="nav-link">Металопластикових вікон та дверей</a></li>
+                    <li><a href="/service" className="nav-link">Броньованих дверей</a></li>
+                    <li><a href="/service" className="nav-link">Воріт та ролет</a></li>
+                  </ul>
+                </div>
+              </div>
             </li>
             <li>
               <a href="/window" className="nav-link">
-                вікна
+              Металопластикові  вікна
               </a>
             </li>
             <li>
-              <a href="/door" className="nav-link">
-                двері
-              </a>
+              <div className="nav-link drop-list">
+                двері <span className="treangel">⏷</span>
+                <div className="drop-down-list-wrap">
+                  <ul className="drop-down-list">
+                    <li>
+                      <a href="#" className="nav-link">
+                        Металопластикові двері
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link">
+                        Броньовані двері{" "}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link">
+                        Міжкімнатні двері
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link">
+                        Фурнітура для дверей
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </li>
             <li>
-              <a href="/garage" className="nav-link">
-                ворота
-              </a>
+              <div className="nav-link drop-list">
+                ворота <span className="treangel">⏷</span>
+                <div className="drop-down-list-wrap">
+                  <ul className="drop-down-list">
+                    <li>
+                      <a href="/garage" className="nav-link">
+                        Гаражні ворота
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/garage" className="nav-link">
+                        Відкатні ворота
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/garage" className="nav-link">
+                        Фурнітура для воріт
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </li>
             <li>
-              <a href="/jaluzi" className="nav-link">
-                жалюзі
-              </a>
+              <div  className="nav-link drop-list">
+                жалюзі та ролети <span className="treangel">⏷</span>
+                <div className="drop-down-list-wrap">
+                  <ul className="drop-down-list">
+                    <li><a href="/jaluzi" className="nav-link">жалюзі</a></li>
+                    <li><a href="/jaluzi" className="nav-link">ролети</a></li>
+                    <li><a href="/jaluzi" className="nav-link">штори вертикальні</a></li>
+                    <li><a href="/jaluzi" className="nav-link">штори горизонтальні</a></li>
+                  </ul>
+                </div>
+              </div>
             </li>
             <li>
-              <a href="/grid" className="nav-link">
-                москітні сітки
-              </a>
+              <div className="nav-link drop-list">
+                Комплектуючі для вікон <span className="treangel">⏷</span>
+                <div className="drop-down-list-wrap">
+                  <ul className="drop-down-list">
+                    <li><a href="/grid" className="nav-link">Підвіконня</a></li>
+                    <li><a href="/grid" className="nav-link">Відливи</a></li>
+                    <li><a href="/grid" className="nav-link">Москітні сітки</a></li>
+                    <li><a href="/grid" className="nav-link">Фурнітура</a></li>
+                  </ul>
+                </div>
+              </div>
             </li>
-            <li>
-              <a href="/pidvikonnia" className="nav-link">
-                підвіконня
-              </a>
-            </li>
+           
             <li>
               <a href="/contact" className="nav-link">
                 контакти
@@ -75,68 +137,64 @@ function Header() {
         </nav>
       </div>
       <div className="mob-menu">
-        
-          <a href="#" className={`main-link  ${pacifico.className}`}>
-            Віконний майстер
-          </a>
-          <button
-            type="button"
-            className="mob-menu-btn"
-            onClick={() => setShowMobMenu(!showMobMenu)}
-          >
-            {showMobMenu ? <CloseIcon /> : <Burger />}
-          </button>
+        <a href="#" className={`main-link  ${pacifico.className}`}>
+          Віконний майстер
+        </a>
+        <button
+          type="button"
+          className="mob-menu-btn"
+          onClick={() => setShowMobMenu(!showMobMenu)}
+        >
+          {showMobMenu ? <CloseIcon /> : <Burger />}
+        </button>
 
-          <nav className={`mob-menu-nav ${showMobMenu ? "activeModal" : null}`}>
-            <ul className="mob-menu-list">
-              <li>
-                <a href="#" className="nav-link">
-                  ГОЛОВНА
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  РЕМОНТ ТА ОБСЛУГОВУВАННЯ
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  ВІКНА
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  ДВЕРІ
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  ВОРОТА
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  ЖАЛЮЗІ
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  МОСКІТНІ СІТКИ
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  ПІДВІКОННЯ
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  КОНТАКТИ
-                </a>
-              </li>
-            </ul>
-          </nav>
-       
+        <nav className={`mob-menu-nav ${showMobMenu ? "activeModal" : null}`}>
+          <ul className="mob-menu-list">
+            <li>
+              <a href="#" className="nav-link">
+                ГОЛОВНА
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                РЕМОНТ ТА ОБСЛУГОВУВАННЯ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                ВІКНА
+              </a>
+            </li>
+            <li>
+              <div className="nav-link">ДВЕРІ</div>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                ВОРОТА
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                ЖАЛЮЗІ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                МОСКІТНІ СІТКИ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                ПІДВІКОННЯ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                КОНТАКТИ
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
